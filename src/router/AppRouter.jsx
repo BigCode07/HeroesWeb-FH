aa6c6e20783f7ce29a35d2062b7781731ef0e2e2
Nicolas Javier Saba <bigcode07@gmail.com>
2025-02-06 14:13:18 -0300

    Se configuraron rutas secundarias

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,19 +1,14 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 
-import { Navbar } from "../ui";
-
-import { MarvelPage, DcPage } from "../heroes";
 import { LoginPage } from "../auth";
+import { HeroesRoutes } from "../heroes";
 
 export const AppRouter = () => {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/marvel" />} />
-        <Route path="marvel" element={<MarvelPage />} />
-        <Route path="dc" element={<DcPage />} />
         <Route path="login" element={<LoginPage />} />
+        <Route path="/*" element={<HeroesRoutes />} />
       </Routes>
     </>
   );
